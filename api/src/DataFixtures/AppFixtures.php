4922349dc3a927048fefead0b19617da781387e7
jordan wakelam <jwakelams@gmail.com>
2024-10-16 00:23:45 +0200

    ajout de la dép verify email bundle je vais éssayer de faire un controler pou avoir enregistrement

--- api/src/DataFixtures/AppFixtures.php
+++ api/src/DataFixtures/AppFixtures.php
@@ -2,16 +2,45 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Loader\ObjectLoader;
 
 class AppFixtures extends Fixture
 {
+    // private ObjectManager $manager;
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
     public function load(ObjectManager $manager): void
     {
         // $product = new Product();
         // $manager->persist($product);
+        $this->addAcount($manager);
 
         $manager->flush();
+    }
+
+    private function addAcount(ObjectManager $manager): void
+    {
+        $user = new User;
+
+        // $passwordHasher = new UserPasswordHasherInterface;
+
+        $userData = [
+            "email" => "jwakelams@gmail.com",
+            "password" => $this->passwordHasher->hashPassword($user, "4321")
+        ];
+
+        $user
+            ->setEmail($userData["email"])
+            ->setPassword($userData["password"]);
+
+        $manager->persist($user);
     }
 }
